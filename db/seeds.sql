57c2376f2ea5d6ee3f85357e7c3570855d373d24
iambryantran <139257318+iambryantran@users.noreply.github.com>
2023-11-09 21:35:44 -0600

    add table structure and starts seed

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO departments (name)
+VALUES  (leadership)
+        (operations),
+        (tech),
+        (finance);
+
+INSERT INTO roles (title, salary, department_id)
+VALUES  ('CEO', 140000, 1),
+        ('Ops', 40000, 2),
+        ('IT', 90000, 3),
+        ('Accountant', 75000, 4);
